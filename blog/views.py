1a4188c1866a265adf2e52e1d04d050cf8c3134f
Kryslte Berry <krystle.monet@gmail.com>
2021-12-01 22:26:53 -0500

    first commit

---
+++ blog/views.py
@@ -0,0 +1,43 @@
+from django.urls.base import reverse, reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView
+
+from .models import Post
+from django.urls import reverse_lazy
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "blog_list.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "blog_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "blog_new.html"
+    fields = ["title", "body", "author"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "blog_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "blog_delete.html"
+    success_url = reverse_lazy("blog_list")
